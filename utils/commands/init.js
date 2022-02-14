117139549ab7cf667a0f7bd4d659ddd4909c4966
Elias Wahl <Eliasruben2204@gmail.com>
2022-02-14 23:31:35 +0100

    bugfixes

--- utils/commands/init.js
+++ utils/commands/init.js
@@ -1,16 +1,15 @@
 const fs = require('fs')
 
 module.exports = async (token) => {
-	const filename = '../../secrets.json';
+	const filename = '/Users/elias_wahl/argus-cli/utils/commands/secrets.json';
 	const file = require(filename);
-
 	file.api.token = token
 
 	fs.writeFile(filename, JSON.stringify(file, null, 2), (err) => {
 		if (err) {
 			return console.log(err);
 		} else {
-			console.log('writin secrets...');
+			console.log('writing secrets...');
 			console.log(JSON.stringify(file));
 		} 
 	});

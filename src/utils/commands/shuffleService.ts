fa30230e9c700c5ebccafb3e53bd558645b8e6ea
Elias Wahl <Eliasruben2204@gmail.com>
2022-02-19 01:25:06 +0100

    added cheerio scraping support

--- src/utils/commands/shuffleService.ts
+++ src/utils/commands/shuffleService.ts
@@ -5,7 +5,7 @@ import { fetchData } from '../helper/webcrawlerHelper'
 
 export default async () => {
     //fill in your own
-    const url = "https://finance.yahoo.com/screener/9464e970-083b-42e5-9e1c-19e745588dac?count=100&offset=0";
+    const url = "";
     //const url ="https://www.iban.com/exchange-rates";
 
     fetchData(url).then( (res) => {

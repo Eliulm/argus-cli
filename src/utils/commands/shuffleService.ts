12e508ca9b831557f4714f5e807e7531d63a7f07
Elias Wahl <Eliasruben2204@gmail.com>
2022-03-14 00:08:54 +0100

    restructured shuffle function

--- src/utils/commands/shuffleService.ts
+++ src/utils/commands/shuffleService.ts
@@ -12,19 +12,20 @@ export default async (amount: number, market: Market) => {
     const url = {"de": "https://de.finance.yahoo.com/most-active/?offset=0&count=100",
                 "us": "https://finance.yahoo.com/most-active/?offset=0&count=100"};
 
-    
-    fetchData(market == "us" ? url["us"] : url["de"]).then( (res) => {
-        const html: string = res.data;
-        const $: any = cheerio.load(html);
-        const tickers = filterData($, amount, market);
-
-        console.log("Market: " + market, "| Amount: " + amount + "\n");
-        for (const ticker of tickers) {
-            console.log(ticker);
-        } 
-
-        if (true) {
-            writeCache(tickers, market);
-        }
-    }).catch((err) => console.log(err));
+    //If last cache is older than a day:
+    if (getCache(date, de).day < today) {
+        //TODO: clear tickers
+        fetchData(market == "us" ? url["us"] : url["de"]).then( (res) => {
+            const html: string = res.data;
+            const $: any = cheerio.load(html);
+            const tickers = filterData($, amount, market);
+            //TODO: Write tickers to cache
+            console.log("Market: " + market, "| Amount: " + amount + "\n");
+            for (const ticker of tickers) {
+                console.log(ticker);
+            }
+        }).catch((err) => console.log(err));
+    } else {
+        //TODO: retrieve tickers from cache.
+    }
 }
